39194c173fdbd55a1f5439a5db353e0ce7d26687
rajatlilani <rajatlilani@gmail.com>
2022-05-21 18:17:27 +0530

    comit2

---
+++ src/About.js
@@ -0,0 +1,24 @@
+import './about.css';
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className="testing">
+            <h1>The ol and ul elements</h1>
+
+            <p>The ol element defines an ordered list:</p>
+            <ol>
+                <li>Coffee</li>
+                <li>Tea</li>
+                <li>Milk</li>
+            </ol>
+
+            <p>The ul element defines an unordered list:</p>
+            <ul>
+                <li>Coffee</li>
+                <li>Tea</li>
+                <li>Milk</li>
+            </ul>
+        </div>
+    )
+}
